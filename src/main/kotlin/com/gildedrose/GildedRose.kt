6087427e931cc34215b737bfcb75d7a54ad13db9
gregorynowik <gregorynowik@gmail.com>
2020-07-10 14:32:57 +0200

    Refactor GildedRose.kt for better readability
    gather all ifs into a when case
    deleted unneeded isRegularItem()

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -17,100 +17,73 @@ class GildedRose(var items: Array<Item>) {
             // REGULAR
             // SULFURAS
 
+            when (item.name) {
 
-            /*
-            if item is not AGED BRIE, BACKSTAGE PASS or SULFURAS (simplified : is REGULAR)
-            and quality > 0
-            decrement quality by 1
-             */
-            if (isRegularItem(item)) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1
-                }
-            }
-
-            /*
-            if item is AGED_BRIE or BACKSTAGE_PASS
-            and quality is lower than 50
-            increment quality by 1
-             */
-            if (item.name == AGED_BRIE_NAME
-                    || item.name == BACKSTAGE_PASS_NAME) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                }
-            }
-
-            /*
-            if item is BACKSTAGE_PASS
-            and quality is lower than 50
-            increment quality by 1
-            a second time if sellin < 10
-            and a third time if sellin is lower than 5
-             */
-
-            if (item.name == BACKSTAGE_PASS_NAME) {
-
-                if (item.sellIn < 11) {
+                AGED_BRIE_NAME -> {
+                    // increment quality, cannot be higher than 50
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1
+                        item.quality++
+                    }
+                    //decrement sellIn
+                    item.sellIn--
+
+                    // if sellIn is nagative and item quality lower than 50, increment quality
+                    if (item.sellIn < 0 && item.quality < 50) {
+                        item.quality++
                     }
                 }
 
-                if (item.sellIn < 6) {
+                BACKSTAGE_PASS_NAME -> {
+                    // increment quality, cannot be higher than 50
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1
+                        item.quality++
                     }
+
+                    // twice if sellIn <11
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality++
+                        }
+                    }
+
+                    // three times is sellin <6
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality++
+                        }
+                    }
+
+                    //decrement sellIn
+                    item.sellIn--
+
+                    // quality drops to 0 if sellIn is negative
+                    if (item.sellIn < 0) {
+                        item.quality = 0
+                    }
+
+
                 }
-            }
 
-            /*
-             for all items except SULFURAS
-             decrement sellIn
-          */
-            if (item.name != SULFURAS_NAME) {
-                item.sellIn = item.sellIn - 1
-            }
+                SULFURAS_NAME -> {
 
-            /*
-            if item is AGED_BRIE
-            if sellIn is nagative
-            and item quality lower than 50
-            increment quality
-             */
-            if (item.name == AGED_BRIE_NAME) {
-                if (item.sellIn < 0 && item.quality < 50) {
-                    item.quality = item.quality + 1
                 }
-            }
 
-            /*
-            if item is not AGED_BRIE, BACKSTAGE_PASS or SUFURAS (simplified : is REGULAR)
-            if quality is negative and sellIn is positive
-            decrement quality
-             */
-            if (isRegularItem(item)) {
-                if (item.sellIn < 0 && item.quality > 0) {
-                    item.quality = item.quality - 1
-                }
-            }
+                //REGULAR
+                else -> {
+                    // if quality is positive, decrement
+                    if (item.quality > 0) {
+                        item.quality--
+                    }
+                    //decrement sellIn
+                    item.sellIn--
 
-            /*
-            for BACKSTAGE_PASS
-            if sellIn is negative, item quality = 0
-             */
-            if (item.name == BACKSTAGE_PASS_NAME) {
-                if (item.sellIn < 0) {
-                    item.quality = 0
+                    // if sellIn is negative and item quality is greater than 0, decrement quality
+                    if (item.sellIn < 0 && item.quality > 0) {
+                        item.quality--
+                    }
                 }
             }
         }
-    }
-
-    private fun isRegularItem(item: Item): Boolean {
-        return item.name != SULFURAS_NAME
-                && item.name != AGED_BRIE_NAME
-                && item.name != BACKSTAGE_PASS_NAME
     }
 
 }
